669d95c8f7cdd38544917520ace210bb158787f0
prathum08 <prathumbhangadia08@gmail.com>
2024-09-18 09:54:18 +0530

    Insertion in array

---
+++ Arrays/deletionInArray.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int deleteElement(int arr[] , int n , int x){
+    int i;
+    for(int i = 0 ; i < n ; i++){
+        if(arr[i] == x){
+            break;
+        }
+    }
+
+    if(i == n){
+        return n;
+    }
+
+    for(int j = i ; j < n - 1 ; j++){
+        arr[j] = arr[j+1];
+    }
+
+    return (n-1);
+}
+
+int main(){
+
+    int arr[] = {26 ,35 ,98 ,45 ,56};
+
+    int sizeAr = sizeof(arr) / sizeof(int);
+
+    cout << "Size before Deletion:- "<< sizeAr << endl;
+
+    int sizeDeleted = deleteElement(arr , sizeAr , 98);
+
+    cout << "Size after Deletion:- " << sizeDeleted << endl;
+
+    
+
+}
