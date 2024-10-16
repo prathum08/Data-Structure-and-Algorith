d9607fdc65cbf144cb1ef50e747156b6b86b8f37
prathum08 <prathumbhangadia08@gmail.com>
2024-10-16 15:25:46 +0530

    Rain Trapping Problem

---
+++ Arrays/TrappingRainWater.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int trapWater(int arr[] , int n){
+    int res = 0;
+    int rmax[n];
+    int lmax[n];
+
+    lmax[0] = arr[0];
+    for(int i = 1 ; i < n ; i++){
+        lmax[i] = max(arr[i] , lmax[i-1]);
+    }
+
+    rmax[n-1] = arr[n-1];
+    for(int i = n -2 ; i >= 0 ; i--){
+        rmax[i] = max(arr[i] ,rmax[i+1] );
+    }
+
+    for(int i = 1 ; i < n -1 ; i++){
+        res = res + (min(lmax[i] , rmax[i]) - arr[i]);
+    }
+
+    return res;
+}
+
+int main(){
+    int arr[] = {2,0,2};
+    int size = sizeof(arr)/sizeof(int);
+
+    trapWater(arr , size);
+
+}
