ccaa1b8c39544f319e5ac6913404ae701136cfbb
prathum08 <prathumbhangadia08@gmail.com>
2025-06-08 22:32:21 +0530

    Roman to integer

---
+++ String/Roman_to_interger.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char , int> mpp{
+            {'I' , 1},
+            {'V' , 5},
+            {'X' , 10},
+            {'L' , 50},
+            {'C' , 100},
+            {'D' , 500},
+            {'M' , 1000},
+
+        };
+
+        int result = 0;
+
+        for(int i = 0 ; i < s.size() ; i++){
+            if(mpp[s[i]] < mpp[s[i+1]]){
+                result -= mpp[s[i]]; 
+            }
+            else{
+                result += mpp[s[i]];
+            }
+        }
+
+        return result;
+    }
+};
