eeb2577caa3d12edaae60ea0d3b9b141cf125fb5
prathum08 <prathumbhangadia08@gmail.com>
2025-06-18 21:33:30 +0530

    Daily Temperature

---
+++ Stack/daily_temperature.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        //Monotonic Stack
+        int n = temperatures.size();
+        stack<int> st;
+        vector<int> res(n , 0);
+
+        //Backward Traversal to check whether greater temp is present or not
+        for(int i = n - 1 ; i >= 0 ; i--){
+            //poping out the element if it is less the the current element
+            while(!st.empty() && temperatures[i] >= temperatures[st.top()]){
+                st.pop();
+            }
+            if(st.empty()){
+                res[i] = 0;
+            }
+            else{
+                res[i] = st.top() - i;
+            }
+
+            st.push(i);
+        }
+
+        return res;
+    }
+};
