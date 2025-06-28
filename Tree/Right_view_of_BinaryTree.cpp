98457993d8d21acbc3a0caa9871a0d3e6af9960f
prathum08 <prathumbhangadia08@gmail.com>
2025-06-28 10:42:29 +0530

    Right view of the tree

---
+++ Tree/Right_view_of_BinaryTree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL){
+            return {};
+        }
+
+        vector<int> res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int qSize = q.size();
+
+            for(int i = 0 ; i < qSize ; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                if(i == qSize - 1){
+                    res.push_back(temp -> val);
+                }
+                
+                if(temp -> left != NULL){
+                    q.push(temp -> left);
+                }
+                if(temp -> right != NULL){
+                    q.push(temp -> right);
+                }
+            }
+        }
+        return res;
+
+        
+    }
+};
