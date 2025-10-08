e96cfd56fcc73336d292e059090141273dbcb66b
prathum08 <prathumbhangadia08@gmail.com>
2025-10-08 15:20:35 +0530

    Subset Sum Problem

---
+++ Dynamic programming/Subset_sum_problem.cpp
@@ -0,0 +1,30 @@
+// this is the modification of the knapsack problem
+// Top down approach
+//Tabluation
+
+
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int n = arr.size();
+        vector<vector<bool>> dp(n + 1 , vector<bool>(sum + 1 , false));
+        
+        for(int i = 0 ; i < n + 1 ; i++){
+            dp[i][0] = true;
+        }
+        for(int i = 1 ; i < n + 1 ;i++){
+            for(int j = 1 ; j < sum + 1 ; j++){
+                if(arr[i - 1] <= j){
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i -1][j];
+                }
+                else if(arr[i - 1] > j){
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        
+        return dp[n][sum];
+    }
+};
